4c1691e59b6bf14319c779e8b38a30650e497e47
Nick DeJesus <ndejesus1227@gmail.com>
2018-12-03 13:35:41 +0100

    many things

--- csvparser.js
+++ csvparser.js
@@ -16,27 +16,27 @@ const GIF_PATH = path.join(__dirname, 'T7C Move List with GIF URLs (CSV File).cs
 const MAPPING = require('./defaultmapping.json');
 
 
-const parseChar = function(names, movelistFN) {
+const parseChar = function (names, movelistFN) {
 	const moveListPath = path.join(CSV_DIR, movelistFN);
 
 	fs.readFile(moveListPath, (err, moveListCSVRaw) => {
-		if(err) {
+		if (err) {
 			console.log(err);
 			return;
 		}
 
 		fs.readFile(GIF_PATH, (err, gifsCSVRaw) => {
-			if(err) {
+			if (err) {
 				console.log(err);
 				return;
 			}
 
-			parse(gifsCSVRaw, {columns: true}, (err, gifsCSV) => {
+			parse(gifsCSVRaw, { columns: true }, (err, gifsCSV) => {
 
 				//Reformat the gifs to store url by notation key
 				let gifsObj = gifsCSV.reduce((acc, rowObj, index) => {
-					if(rowObj['Character'] == names.displayName) {
-						acc[rowObj['Move Input']] = {move_name: rowObj['Move Name'], preview_url: rowObj['GIF URL']};
+					if (rowObj['Character'] == names.displayName) {
+						acc[rowObj['Move Input']] = { move_name: rowObj['Move Name'], preview_url: rowObj['GIF URL'] };
 					}
 
 					return acc;
@@ -53,12 +53,12 @@ const parseChar = function(names, movelistFN) {
 						movelist: moveListObj
 					};
 
-					fs.writeFile(path.join(JSON_DIR, names.label + '.json'), JSON.stringify(charObj, null, '\t'), 'utf8', (err) => { 
-						if(err) console.log('Writing failed. I hate my life: ', err);
+					fs.writeFile(path.join(JSON_DIR, names.label + '.json'), JSON.stringify(charObj, null, '\t'), 'utf8', (err) => {
+						if (err) console.log('Writing failed. I hate my life: ', err);
 						else console.log("Yo you did it, good job");
 					});
 				});
-				
+
 			})
 
 		});
@@ -67,17 +67,17 @@ const parseChar = function(names, movelistFN) {
 
 };
 
-const moveListToJson = function(moveListCSVArray, gifsObj, label) {
+const moveListToJson = function (moveListCSVArray, gifsObj, label) {
 	let mappedCols = [];
 	let dividerCount = 0;
 
 	//console.log(gifsObj);
 	return moveListCSVArray.reduce((acc, rowArray, index) => {
-		if(index == 0) {
+		if (index == 0) {
 			const colsToMap = Object.keys(MAPPING);
-			
-			for(let i = 0; i < rowArray.length; i++) {
-				if(colsToMap.includes(rowArray[i])) {
+
+			for (let i = 0; i < rowArray.length; i++) {
+				if (colsToMap.includes(rowArray[i])) {
 					mappedCols[i] = rowArray[i];
 				}
 			}
@@ -94,21 +94,21 @@ const moveListToJson = function(moveListCSVArray, gifsObj, label) {
 			move_name: null
 		};
 
-		for(let i = 0; i < rowArray.length; i++) {
+		for (let i = 0; i < rowArray.length; i++) {
 			//Checker for empty section divider rows
-			if(sectionDivider && i > 0 && rowArray[i])
+			if (sectionDivider && i > 0 && rowArray[i])
 				sectionDivider = false;
 
 			//Only storing data listed in the mapping file
-			if(mappedCols[i]) {
+			if (mappedCols[i]) {
 				const col = MAPPING[mappedCols[i]]
 				rowObj[col] = rowArray[i];
 
 				//GIF data stored by notation so also searching here
-				if(col == "notation") {
+				if (col == "notation") {
 					let gifData = gifsObj[rowArray[i]];
 
-					if(gifData) {
+					if (gifData) {
 						rowObj['preview_url'] = gifData.preview_url;
 						rowObj['move_name'] = gifData.move_name;
 					}
@@ -117,14 +117,14 @@ const moveListToJson = function(moveListCSVArray, gifsObj, label) {
 
 		}
 
-		rowObj['id'] = label + (index - dividerCount);
-		
-		if(!sectionDivider) {
+		rowObj['id'] = `${label}_` + (index - dividerCount);
+
+		if (!sectionDivider) {
 			acc.push(rowObj);
 		} else {
 			dividerCount++;
 		}
-		
+
 		return acc;
 	}, []);
 
